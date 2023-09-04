3293334880c44903823c7ce4dfd865ee52ea5021
LenaWang03 <xianna03@gmail.com>
2023-09-04 02:24:41 -0700

    added beginnings of landing page content and styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,21 @@
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import landingBG from "../imgs/landingBG.svg";
+import landingCat from "../imgs/landingCat.svg";
 
 const Home = () => {
   return (
     <>
       <NavBar />
-      <main className="homeContent">hi</main>
+      <main className="homeContent">
+        <div className="homeText">
+          <h1>Hello, I'm </h1>
+          <span className="hugeFont">Lena Wang.</span>
+        </div>
+        <img alt="landingBG" src={landingBG} />
+        <img className="landingCat" alt="landingCat" src={landingCat} />
+      </main>
+
       <Footer />
     </>
   );
